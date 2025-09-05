894eccfaa807ea365ac33e997e4f9cce25ec730f
deroch22 <115877456+deroch22@users.noreply.github.com>
2025-09-05 12:57:35 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/sayur5/', // ← repo name
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-  build: { outDir: 'dist', sourcemap: false },
+export default defineConfig(() => {
+  // kalau dulu kamu pakai base dinamis, pastikan build GH memberi DEPLOY_TARGET=gh
+  return {
+    plugins: [react()],
+    base: '/sayur5/',                               // <— WAJIB untuk GitHub Pages (project repo)
+    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+    build: { outDir: 'dist' },
+  }
 })
