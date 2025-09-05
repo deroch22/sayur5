6c1006b751cfd6111fb204ded8a516748e777075
deroch22 <115877456+deroch22@users.noreply.github.com>
2025-09-05 12:42:37 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import SayurSerbaLima from "./SayurSerbaLima.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<SayurSerbaLima />} />
+        <Route path="/admin" element={<SayurSerbaLima adminOnly />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
