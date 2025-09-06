503049030714ee9f95df2035cc18b2ce89709f8b
deroch22 <115877456+deroch22@users.noreply.github.com>
2025-09-06 15:31:17 +0700

    Update upload.js

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,6 +1,7 @@
+// src/utils/upload.js
 export async function uploadToR2(file, pin) {
   const fd = new FormData();
-  fd.append("file", file);
+  fd.append("file", file, file.name);
   const r = await fetch("/api/upload", {
     method: "POST",
     body: fd,
